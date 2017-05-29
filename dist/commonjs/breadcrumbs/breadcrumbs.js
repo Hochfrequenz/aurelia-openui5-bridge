94b12a7a31a5d3670409b12edbd83f739873b384
Joscha Metze <joscha.metze@hochfrequenz.de>
2017-05-29 22:34:08 +0200

    feat(ui): add breadcrumbs

---
+++ dist/commonjs/breadcrumbs/breadcrumbs.js
@@ -0,0 +1,137 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Ui5BreadCrumbs = undefined;
+
+var _dec, _dec2, _dec3, _dec4, _class, _desc, _value, _class2, _descriptor, _descriptor2;
+
+var _aureliaTemplating = require('aurelia-templating');
+
+var _aureliaDependencyInjection = require('aurelia-dependency-injection');
+
+var _attributeManager = require('../common/attributeManager');
+
+var _attributes = require('../common/attributes');
+
+var _control = require('../control/control');
+
+function _initDefineProp(target, property, descriptor, context) {
+  if (!descriptor) return;
+  Object.defineProperty(target, property, {
+    enumerable: descriptor.enumerable,
+    configurable: descriptor.configurable,
+    writable: descriptor.writable,
+    value: descriptor.initializer ? descriptor.initializer.call(context) : void 0
+  });
+}
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) {
+  var desc = {};
+  Object['ke' + 'ys'](descriptor).forEach(function (key) {
+    desc[key] = descriptor[key];
+  });
+  desc.enumerable = !!desc.enumerable;
+  desc.configurable = !!desc.configurable;
+
+  if ('value' in desc || desc.initializer) {
+    desc.writable = true;
+  }
+
+  desc = decorators.slice().reverse().reduce(function (desc, decorator) {
+    return decorator(target, property, desc) || desc;
+  }, desc);
+
+  if (context && desc.initializer !== void 0) {
+    desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
+    desc.initializer = undefined;
+  }
+
+  if (desc.initializer === void 0) {
+    Object['define' + 'Property'](target, property, desc);
+    desc = null;
+  }
+
+  return desc;
+}
+
+function _initializerWarningHelper(descriptor, context) {
+  throw new Error('Decorating class property failed. Please ensure that transform-class-properties is enabled.');
+}
+
+var Ui5BreadCrumbs = exports.Ui5BreadCrumbs = (_dec = (0, _aureliaTemplating.customElement)('ui5-breadcrumbs'), _dec2 = (0, _aureliaDependencyInjection.inject)(Element), _dec3 = (0, _aureliaTemplating.bindable)(), _dec4 = (0, _aureliaTemplating.bindable)(), _dec(_class = _dec2(_class = (_class2 = function () {
+  function Ui5BreadCrumbs(element) {
+    _classCallCheck(this, Ui5BreadCrumbs);
+
+    this._crumbs = null;
+
+    _initDefineProp(this, 'ui5Id', _descriptor, this);
+
+    _initDefineProp(this, 'currentLocationText', _descriptor2, this);
+
+    this.element = element;
+  }
+
+  Ui5BreadCrumbs.prototype.defaultFunc = function defaultFunc() {};
+
+  Ui5BreadCrumbs.prototype.attached = function attached() {
+    var attributeManager = new _attributeManager.AttributeManager(this.element);
+    var params = {
+      currentLocationText: this.currentLocationText
+    };
+    if (this.ui5Id) this._crumbs = new sap.m.Breadcrumbs(this.ui5Id, params);else this._crumbs = new sap.m.Breadcrumbs(params);
+    $(this.element).parents("[ui5-container]")[0].au.controller.viewModel.addChild(this._crumbs, this.element);
+    attributeManager.addAttributes({ "ui5-container": '' });
+  };
+
+  Ui5BreadCrumbs.prototype.addChild = function addChild(child, elem) {
+    var path = $(elem).parentsUntil(this.element);
+    for (var _iterator = path, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
+      if (_isArray) {
+        if (_i >= _iterator.length) break;
+        elem = _iterator[_i++];
+      } else {
+        _i = _iterator.next();
+        if (_i.done) break;
+        elem = _i.value;
+      }
+
+      if (elem.localName == 'links') {
+        this._crumbs.addLink(child);break;
+      }
+    }
+  };
+
+  Ui5BreadCrumbs.prototype.removeChild = function removeChild(child, elem) {
+    var path = $(elem).parentsUntil(this.element);
+    for (var _iterator2 = path, _isArray2 = Array.isArray(_iterator2), _i2 = 0, _iterator2 = _isArray2 ? _iterator2 : _iterator2[Symbol.iterator]();;) {
+      if (_isArray2) {
+        if (_i2 >= _iterator2.length) break;
+        elem = _iterator2[_i2++];
+      } else {
+        _i2 = _iterator2.next();
+        if (_i2.done) break;
+        elem = _i2.value;
+      }
+
+      if (elem.localName == 'links') {
+        this._crumbs.removeLink(child);break;
+      }
+    }
+  };
+
+  return Ui5BreadCrumbs;
+}(), (_descriptor = _applyDecoratedDescriptor(_class2.prototype, 'ui5Id', [_dec3], {
+  enumerable: true,
+  initializer: function initializer() {
+    return null;
+  }
+}), _descriptor2 = _applyDecoratedDescriptor(_class2.prototype, 'currentLocationText', [_dec4], {
+  enumerable: true,
+  initializer: function initializer() {
+    return null;
+  }
+})), _class2)) || _class) || _class);
---
+++ dist/dev/breadcrumbs/breadcrumbs.js
@@ -0,0 +1,150 @@
+'use strict';
+
+System.register(['aurelia-templating', 'aurelia-dependency-injection', '../common/attributeManager', '../common/attributes', '../control/control'], function (_export, _context) {
+  "use strict";
+
+  var bindable, customElement, noView, inject, AttributeManager, getBooleanFromAttributeValue, Ui5Control, _dec, _dec2, _dec3, _dec4, _class, _desc, _value, _class2, _descriptor, _descriptor2, Ui5BreadCrumbs;
+
+  function _initDefineProp(target, property, descriptor, context) {
+    if (!descriptor) return;
+    Object.defineProperty(target, property, {
+      enumerable: descriptor.enumerable,
+      configurable: descriptor.configurable,
+      writable: descriptor.writable,
+      value: descriptor.initializer ? descriptor.initializer.call(context) : void 0
+    });
+  }
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) {
+    var desc = {};
+    Object['ke' + 'ys'](descriptor).forEach(function (key) {
+      desc[key] = descriptor[key];
+    });
+    desc.enumerable = !!desc.enumerable;
+    desc.configurable = !!desc.configurable;
+
+    if ('value' in desc || desc.initializer) {
+      desc.writable = true;
+    }
+
+    desc = decorators.slice().reverse().reduce(function (desc, decorator) {
+      return decorator(target, property, desc) || desc;
+    }, desc);
+
+    if (context && desc.initializer !== void 0) {
+      desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
+      desc.initializer = undefined;
+    }
+
+    if (desc.initializer === void 0) {
+      Object['define' + 'Property'](target, property, desc);
+      desc = null;
+    }
+
+    return desc;
+  }
+
+  function _initializerWarningHelper(descriptor, context) {
+    throw new Error('Decorating class property failed. Please ensure that transform-class-properties is enabled.');
+  }
+
+  return {
+    setters: [function (_aureliaTemplating) {
+      bindable = _aureliaTemplating.bindable;
+      customElement = _aureliaTemplating.customElement;
+      noView = _aureliaTemplating.noView;
+    }, function (_aureliaDependencyInjection) {
+      inject = _aureliaDependencyInjection.inject;
+    }, function (_commonAttributeManager) {
+      AttributeManager = _commonAttributeManager.AttributeManager;
+    }, function (_commonAttributes) {
+      getBooleanFromAttributeValue = _commonAttributes.getBooleanFromAttributeValue;
+    }, function (_controlControl) {
+      Ui5Control = _controlControl.Ui5Control;
+    }],
+    execute: function () {
+      _export('Ui5BreadCrumbs', Ui5BreadCrumbs = (_dec = customElement('ui5-breadcrumbs'), _dec2 = inject(Element), _dec3 = bindable(), _dec4 = bindable(), _dec(_class = _dec2(_class = (_class2 = function () {
+        function Ui5BreadCrumbs(element) {
+          _classCallCheck(this, Ui5BreadCrumbs);
+
+          this._crumbs = null;
+
+          _initDefineProp(this, 'ui5Id', _descriptor, this);
+
+          _initDefineProp(this, 'currentLocationText', _descriptor2, this);
+
+          this.element = element;
+        }
+
+        Ui5BreadCrumbs.prototype.defaultFunc = function defaultFunc() {};
+
+        Ui5BreadCrumbs.prototype.attached = function attached() {
+          var attributeManager = new AttributeManager(this.element);
+          var params = {
+            currentLocationText: this.currentLocationText
+          };
+          if (this.ui5Id) this._crumbs = new sap.m.Breadcrumbs(this.ui5Id, params);else this._crumbs = new sap.m.Breadcrumbs(params);
+          $(this.element).parents("[ui5-container]")[0].au.controller.viewModel.addChild(this._crumbs, this.element);
+          attributeManager.addAttributes({ "ui5-container": '' });
+        };
+
+        Ui5BreadCrumbs.prototype.addChild = function addChild(child, elem) {
+          var path = $(elem).parentsUntil(this.element);
+          for (var _iterator = path, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
+            if (_isArray) {
+              if (_i >= _iterator.length) break;
+              elem = _iterator[_i++];
+            } else {
+              _i = _iterator.next();
+              if (_i.done) break;
+              elem = _i.value;
+            }
+
+            if (elem.localName == 'links') {
+              this._crumbs.addLink(child);break;
+            }
+          }
+        };
+
+        Ui5BreadCrumbs.prototype.removeChild = function removeChild(child, elem) {
+          var path = $(elem).parentsUntil(this.element);
+          for (var _iterator2 = path, _isArray2 = Array.isArray(_iterator2), _i2 = 0, _iterator2 = _isArray2 ? _iterator2 : _iterator2[Symbol.iterator]();;) {
+            if (_isArray2) {
+              if (_i2 >= _iterator2.length) break;
+              elem = _iterator2[_i2++];
+            } else {
+              _i2 = _iterator2.next();
+              if (_i2.done) break;
+              elem = _i2.value;
+            }
+
+            if (elem.localName == 'links') {
+              this._crumbs.removeLink(child);break;
+            }
+          }
+        };
+
+        return Ui5BreadCrumbs;
+      }(), (_descriptor = _applyDecoratedDescriptor(_class2.prototype, 'ui5Id', [_dec3], {
+        enumerable: true,
+        initializer: function initializer() {
+          return null;
+        }
+      }), _descriptor2 = _applyDecoratedDescriptor(_class2.prototype, 'currentLocationText', [_dec4], {
+        enumerable: true,
+        initializer: function initializer() {
+          return null;
+        }
+      })), _class2)) || _class) || _class));
+
+      _export('Ui5BreadCrumbs', Ui5BreadCrumbs);
+    }
+  };
+});
+//# sourceMappingURL=../dist/dev/breadcrumbs/breadcrumbs.js.map
